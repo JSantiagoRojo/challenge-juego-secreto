d970a84f623d2254d014e000336b371a50efcca8
Julian Santiago <j.santiago.rojo.conde@gmail.com>
2025-08-11 21:23:28 -0600

    actualizacion app

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = []
+
+  function Ingresaramigos (){
+let amigos = document.getElementById ()
+if (amigos = null) {
+    textoDeDocumento('Ingrese un Nombre');
+}
+    else {
+        textoDeDocumento(amigos)
+    }
+  }
