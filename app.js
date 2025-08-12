0e5c8024ab79adf8a6098f99fcf43a2bfe78dba6
Julian Santiago <j.santiago.rojo.conde@gmail.com>
2025-08-11 22:52:23 -0600

    actualizacion app

--- app.js
+++ app.js
@@ -1,12 +1,40 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = []
 
-  function Ingresaramigos (){
-let amigos = document.getElementById ()
-if (amigos = null) {
-    textoDeDocumento('Ingrese un Nombre');
+  function agregarAmigo()  {                          //Función para agregar nombre de amigo
+  const input = document.getElementById("amigos"); 
+  const nombre = input.value.trim();
+if (!nombre) {                                     //verifica que el campo no esté vacío
+    alert('Por favor ingrese un nombre')
+    return;
 }
-    else {
-        textoDeDocumento(amigos)
-    }
-  }
+    amigos.push(nombre)                            //añade el nombre al array "amigos"
+    input.value = "";
+        const lista = document.getElementById("listaAmigos");
+         lista.innerHTML = ""
+amigos.forEach((nombre) => {
+  const li = document.createElement("li");
+  li.textContent = nombre;
+  lista.appendChild(li);
+});
+}
+
+function sortearAmigo() {                           //Función para sortear amigo
+if (amigos.length === 0) {
+    alert ('Por favor ingrese al menos un nombre')
+    return
+}
+else { 
+    const indice = Math.floor(Math.random() * amigos.length)
+const amigosecreto = amigos[indice]
+const resultado = document.getElementById("resultado")
+resultado.innerHTML = ""
+const li = document.createElement("li")
+li.textContent = amigosecreto
+resultado.appendChild(li)
+}
+}
+
+
+  
+  
